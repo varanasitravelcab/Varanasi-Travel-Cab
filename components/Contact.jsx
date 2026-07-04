21ef0e6d66f3e7a5d40686fe4ee5d6e3bc63e1ff
Kartikey <kartikeypathak08@gmail.com>
2026-07-04 13:43:12 +0530

    changed the ui bg color of contact us

--- components/Contact.jsx
+++ components/Contact.jsx
@@ -31,7 +31,7 @@ const Contact = () => {
 
       <div className="min-h-screen w-screen md:w-full md:mt-10  bg-transparent flex items-center justify-center md:px-6 py-12">
 
-        <div className="w-full  backdrop-blur-xl rounded-3xl shadow-2xl p-6 md:p-8 grid md:grid-cols-2 gap-10 hover:shadow-[0_0_40px_rgba(249,115,22,0.15)] transition-shadow duration-700">
+        <div className="w-full  bg-[#0F172A] rounded-3xl shadow-2xl p-6 md:p-8 grid md:grid-cols-2 gap-10 hover:shadow-[0_0_40px_rgba(249,115,22,0.15)] transition-shadow duration-700">
 
 
           <div className="relative bg-[#F97316] text-white rounded-2xl p-8 md:p-10 flex flex-col justify-between overflow-hidden shadow-lg group hover:scale-[1.01] transition-transform duration-500">
@@ -164,3 +164,5 @@ const SocialIcon = ({ link, children }) => (
 );
 
 export default Contact;
+
+

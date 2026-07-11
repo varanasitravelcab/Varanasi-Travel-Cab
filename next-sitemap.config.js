1cf7f32249cd00d304aa14d0f393c860c9fbeebe
Kartikey <kartikeypathak08@gmail.com>
2026-07-11 15:49:23 +0530

    deleted not required file

--- next-sitemap.config.js
+++
@@ -1,6 +0,0 @@
-/** @type {import('next-sitemap').IConfig} */
-module.exports = {
-  siteUrl: 'https://travelexplores.com',  // ✅ your domain
-  generateRobotsTxt: true,                // also creates robots.txt
-  sitemapSize: 7000,                      // optional
-};
